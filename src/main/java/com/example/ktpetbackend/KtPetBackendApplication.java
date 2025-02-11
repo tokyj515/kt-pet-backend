8113a7595fbbfcd284833252be76647b5585b020
YuJin Kwon <tokyj515@gmail.com>
2025-02-11 09:30:36 +0900

    Initial commit

---
+++ src/main/java/com/example/ktpetbackend/KtPetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktpetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtPetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtPetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ktpetbackend/KtPetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktpetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtPetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
