32b6050ae231794c80d2959cdfda8fecf1b03b5c
YuJin Kwon <tokyj515@gmail.com>
2025-02-11 14:17:43 +0900

    feat: 스웨거 버전 업그레이드

---
+++ src/main/java/com/example/ktpetbackend/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.ktpetbackend;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "SEMTLE API", version = "1.0", description = "SEMTLE API Documentation"),
+        security = @SecurityRequirement(name = "Authorization")
+)
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
